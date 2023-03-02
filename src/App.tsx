7cc10bd53a0437d14ac5a69933764dad0d40dc8d
mjapadilla <mjapadilla@multisyscor.com>
2023-03-02 16:06:34 +0800

    Resolve .gitignore error

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   React.useEffect(() => {
     setState({ name: '3' });
   }, []);
-  const a = '';
+
   return (
     <div className="w-screen h-screen bg-cyan-700 flex">
       <div className="text-white m-auto">
